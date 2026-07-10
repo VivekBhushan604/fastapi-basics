f1f7fbecfc4ff4784f859aabff164fa5a699bcc3
Vivek Bhushan <vivekbhushan604@gmail.com>
2026-07-10 16:42:04 +0530

    Day 22 task and assignment Add student API with path parameters and error handling

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "Vivek Bhushan",
+        "course": "B.Tech CSE"
+    },
+    2: {
+        "name": "KS Aravind",
+        "course": "B.Tech AI"
+    },
+    3: {
+        "name": "Ayushmaan",
+        "course": "B.Tech ECE"
+    }
+}
 
 @app.get("/")
 def home():
@@ -14,3 +28,19 @@ def about():
         "name": "Vivek Bhushan",
         "title": "AI/ML Intern"
     }
+
+
+@app.get("/students")
+def get_students():
+    return students
+
+
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+    if student_id not in students:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Student not found"
+        )
+
+    return students[student_id]
