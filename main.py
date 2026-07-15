c083c188b5d4ec373196f5ae198fb82eaa7f6a38
Vivek Bhushan <vivekbhushan604@gmail.com>
2026-07-15 15:40:26 +0530

    Day 23:  Calculator API using query parameters

--- main.py
+++ main.py
@@ -44,3 +44,32 @@ def get_student(student_id: int):
         )
 
     return students[student_id]
+
+
+@app.get("/calculate")
+def calculate(num1: float, num2: float, operation: str):
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return {"error": "Division by zero is not allowed"}
+        result = num1 / num2
+    elif operation == "power":
+        result = num1 ** num2
+    elif operation == "modulus":
+        if num2 == 0:
+            return {"error": "Modulus by zero is not allowed"}
+        result = num1 % num2
+    else:
+        return {"error": "Invalid operation"}
+
+    return {
+        "num1": num1,
+        "num2": num2,
+        "operation": operation,
+        "result": result
+    }
